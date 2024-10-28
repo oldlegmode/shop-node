579f9df32473acf2ccfeebefd412d98000f524f8
oleg filkin <oleg.filkin.by@gmail.com>
2024-10-28 21:22:51 +0300

    feat: add pagination

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,14 +4,38 @@ const fs = require('fs');
 const path = require('path');
 const PDFDocument = require('pdfkit');
 
+const ITEMS_PER_PAGE = 2;
+
+const getPaginationData = (page = 1, totalItems, items_per_page) => {
+  return {
+    // totalProducts: totalItems,
+    currentPage: page,
+    hasNextPage: items_per_page * page < totalItems,
+    hasPreviousPage: page > 1,
+    nextPage: page + 1,
+    previousPage: page - 1,
+    lastPage: Math.ceil(totalItems / items_per_page)
+  }
+}
+
 exports.getProducts = (req, res, next) => {
+  const page = +req.query.page || 1;
   Product.find()
+    .countDocuments()
+    .then(numProducts => {
+      totalItems = numProducts;
+      
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE)
+    })
     .then(products => {
       console.log(products);
       res.render('shop/product-list', {
         prods: products,
         pageTitle: 'All Products',
-        path: '/products'
+        path: '/products',
+        ...getPaginationData(page, totalItems, ITEMS_PER_PAGE)
       });
     })
     .catch(err => {
@@ -33,16 +57,29 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalItems;
+
   Product.find()
+    .countDocuments()
+    .then(numProducts => {
+      totalItems = numProducts;
+      
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE)
+    })
     .then(products => {
       res.render('shop/index', {
         prods: products,
         pageTitle: 'Shop',
         path: '/',
+        ...getPaginationData(page, totalItems, ITEMS_PER_PAGE)
       });
     })
     .catch(err => {
-      const error = new Error(err)
+      const error = new Error(err);
+      console.log(error)
       error.httpStatusCode = 500;
       return next(error);
     });
